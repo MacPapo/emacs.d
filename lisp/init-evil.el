acbf5a6587c8de12885b062735a33a5dfe158122
MacPapo <jacopocostantini32@gmail.com>
2023-12-28 17:40:05 +0100

    Evil mode added

---
+++ lisp/init-evil.el
@@ -0,0 +1,11 @@
+;;; init-evil.el --- Using VIM bindings ;; -*- lexical-bindings: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package evil
+  :demand t
+  :config
+  (evil-mode 1))
+
+(provide 'init-evil)
+;;; init-evil.el ends here
