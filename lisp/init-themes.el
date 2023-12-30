f2abcf58ff830432b3603187458146ff31d92760
MacPapo <jacopocostantini32@gmail.com>
2023-12-30 11:35:40 +0100

    Optimizing Evil experience

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -5,7 +5,8 @@
 (use-package modus-themes)
 (use-package base16-theme)
 
-(load-theme 'base16-ashes t)
+;;(load-theme 'base16-ashes t)
+(load-theme 'modus-vivendi t)
 
 (set-face-attribute 'default nil :font "Iosevka" :height 110)
 
